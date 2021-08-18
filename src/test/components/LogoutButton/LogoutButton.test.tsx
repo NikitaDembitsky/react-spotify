689a7044cd546bb6d77119cf2608d9dad0bc4d21
NikitaDembitsky <nikita.diembitski@gmail.com>
2021-08-18 22:25:18 +0300

    add new test

---
+++ src/test/components/LogoutButton/LogoutButton.test.tsx
@@ -0,0 +1,26 @@
+import Enzyme, { shallow } from "enzyme";
+import LogoutButton from "../../../components/LogoutButton/LogoutButton";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { Button } from "@material-ui/core";
+import { logOut } from "../../../utils";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+describe("LogoutButton", () => {
+  const wrapper = shallow(<LogoutButton />);
+  it('renders a button with "LogoutButton" as children', () => {
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+  it("text inside p tag", () => {
+    const paragraph = wrapper.find("p");
+    expect(paragraph).toHaveLength(1);
+    expect(paragraph.text()).toEqual("Log Out");
+  });
+
+  it("calls getCodeValue on click", () => {
+    const button = wrapper.find(Button);
+    expect(button.props().onClick).toEqual(logOut);
+  });
+});
